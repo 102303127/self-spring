924c0c831fc50d22fd56f97c4be0f93e541839e6
Zhang Yu <143883582+102303127@users.noreply.github.com>
2024-08-17 22:34:28 +0800

    Update BeanReference.java

--- spring模块/src/main/java/com/zhang/beans/factory/config/BeanReference.java
+++ spring模块/src/main/java/com/zhang/beans/factory/config/BeanReference.java
@@ -13,12 +13,84 @@ public class BeanReference {
 
     private final String beanName;
 
+    private final Class<?> beanType;
+
+    private final boolean toParent;
+
+    private Object source;
+
+
 
     public BeanReference(String beanName) {
-        this.beanName = beanName;
+        this(beanName, false);
     }
 
+
+    public BeanReference(String beanName, boolean toParent) {
+        this.beanName = beanName;
+        this.beanType = null;
+        this.toParent = toParent;
+    }
+
+
+    public BeanReference(Class<?> beanType) {
+        this(beanType, false);
+    }
+
+    public BeanReference(Class<?> beanType, boolean toParent) {
+        this.beanName = beanType.getName();
+        this.beanType = beanType;
+        this.toParent = toParent;
+    }
+
+
     public String getBeanName() {
-        return beanName;
+        return this.beanName;
+    }
+
+
+    public Class<?> getBeanType() {
+        return this.beanType;
+    }
+
+
+    public boolean isToParent() {
+        return this.toParent;
+    }
+
+
+    public void setSource(Object source) {
+        this.source = source;
+    }
+
+
+    public Object getSource() {
+        return this.source;
+    }
+
+
+    @Override
+    public boolean equals( Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof BeanReference)) {
+            return false;
+        }
+        BeanReference that = (BeanReference) other;
+        return (this.beanName.equals(that.beanName) && this.beanType == that.beanType &&
+                this.toParent == that.toParent);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.beanName.hashCode();
+        result = 29 * result + (this.toParent ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return '<' + getBeanName() + '>';
     }
 }
